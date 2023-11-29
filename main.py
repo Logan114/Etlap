e5b7898ebd04eaddde5c3e3c3207b5c67cd4f879
Logan114 <kepirobalazs204@gmail.com>
2023-11-29 13:47:39 +0100

    felkesz

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import etlap
-from etlap import *
 import rendeles
+from rendeles import rendelesek_indexe_lista
+from etlap import *
 etlap.etl()
 etlap.item(levesek,"levesek",levesar)
 etlap.item(foetel,"Főételek",foetelar)
 etlap.item(itallap,"Italok",italar)
 etlap.item(desszertek,"Desszertek",desszertar)
+rendeles.rendeles(levesek,"levesek",levesar)
+rendeles.rendeles(foetel,"Főételek",foetelar)
+rendeles.rendeles(itallap,"italok",italar)
+rendeles.ossz()
