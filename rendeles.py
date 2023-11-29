216e6faf760f40c582400708dcf71cdba2c9f649
Logan114 <kepirobalazs204@gmail.com>
2023-11-29 13:47:20 +0100

    felkesz

--- rendeles.py
+++ rendeles.py
@@ -1,66 +1,26 @@
 import etlap
 from etlap import *
 
-levesek = ["0 - Nem kér semmit", "1 - Húsleves", "2 - Gyümölcsleves", "3 - Bableves"]
-levesar = [0,880,600,1200]
-foetel = ["0 - Nem kér semmit", "1 - Tojásos nokedli", "2 - Rántott hús rizibizivel", "3 - Rántott hús sült krumplival"]
-foetelar = [0,880,1500,2000]
-itallap = ["0 - Nem kér semmit", "1 - Coca Cola", "2 - Kőbányai", "3 - Márka szörp"]
-italar = [0,450,450,350]
-desszertek = ["0 - Nem kér semmit", "1 - Tiramisu", "2 - Nutellás palacsinta", "3 - Túrós palacsinta"]
-desszertar = [0,650,750,250]
+rendelesek_indexe_lista = []
+menu_elemek_lista = [levesek, foetel, itallap ,desszertek]
+menu_ar_lista = [levesar, foetelar, italar, desszertar]
 
-
-def levren():
+def rendeles(etel,cim,etelar):
     print("\n\n")
-    etlap.lev()
-    term = "leves"
-    rendeles1: int = int(input(f"Írd be hányas számú {term}t kéred!(0 ha semmit)"))
-    global leves
-    leves = levesek[rendeles1]
-    global levar
-    levar = levesar[rendeles1]
-    print(f"{leves} - {levar} Ft")
-
-def foren():
-    print("\n\n")
-    etlap.foet()
-    term = "főétel"
-    rendeles2: int = int(input(f"Írd be hányas számú {term}t kéred!(0 ha semmit)"))
-    global foet
-    foet = foetel[rendeles2]
-    global foetar
-    foetar = foetelar[rendeles2]
-    print(f"{foet} - {foetar} Ft")
-
-def itren():
-    print("\n\n")
-    etlap.drink()
-    term = "ital"
-    rendeles3: int = int(input(f"Írd be hányas számú {term}t kéred!(0 ha semmit)"))
-    global ital
-    ital = itallap[rendeles3]
-    global itar
-    itar = italar[rendeles3]
-    print(f"{ital} - {itar} Ft")
-
-def deszren():
-    print("\n\n")
-    etlap.dessert()
-    term = "desszert"
-    rendeles4: int = int(input(f"Írd be hányas számú {term}et kéred!(0 ha semmit)"))
-    global desz
-    desz = desszertek[rendeles4]
-    global desszert_ar
-    desszert_ar = desszertar[rendeles4]
-    print("Válasszon levest! (0-tol 3-ig válasszon)")
-    print(f"{desz} - {desszert_ar} Ft")
+    etlap.item(etel,cim,etelar)
+    rendeles1: int = int(input(f"Írd be hányas számú {cim}t kéred!(0 ha semmit) "))
+    rendelesek_indexe_lista.append(rendeles1)
 
 def ossz():
     etlap.keret(meret,karakter)
     etlap.szoveg_kiiras("*","ÖSSZEGZÉS","*",50)
     etlap.keret(meret, karakter)
-    etlap.etel_kiiras("*",leves,"*",50,levar)
+    n = 0
+    while n < len(menu_elemek_lista):
+        n += 1
+        etlap.etel_kiiras("*",,"*",50,(menu_elemek_lista[(rendelesek_indexe_lista[n])]))
+
+    ''''
     etlap.keret(meret, karakter)
     etlap.etel_kiiras("*",foet,"*",50,foetar)
     etlap.keret(meret, karakter)
@@ -71,4 +31,5 @@ def ossz():
     veg =(levar+foetar+itar+desszert_ar)
     etlap.etel_kiiras("*", "Végösszeg","*",50,veg)
     etlap.keret(meret, karakter)
+    '''''
 
